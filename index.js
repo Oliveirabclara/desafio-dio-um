66e887829e6c211558bf8a1a043fe2fb975e62a3
Clara B. Oliveira <batistaoliveira.clara@gmail.com>
2024-10-21 21:11:38 -0400

    Adicao das Estruturas de decisao

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello, World!")
+let nome = "Rogerio3000"
+let xpJogador = 3456
+let nivel = ""
+
+if(xpJogador <=1000){
+    nivel = "Ferro";
+
+}else if (xpJogador =1001 && xpJogador <=2000){
+    nivel = "Bronze";
+
+}else if (xpJogador =2001 && xpJogador <=5000){
+    nivel = "Prata";
+
+}else if (xpJogador =5001 && xpJogador <=7000){
+    nivel = "Ouro";
+
+}else if (xpJogador =7001 && xpJogador <=8000){
+    nivel = "Platina";
+
+}else if (xpJogador =8001 && xpJogador <=9000){
+    nivel = "Ascendente";
+
+}else if (xpJogador =9001 && xpJogador <=10000){
+    nivel = "Imortal";
+
+}else {
+    nivel = "Radiante";
+
+}
